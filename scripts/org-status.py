5ffb5423eaa91e24f0c4ebb0785f313f11b70f70
nickgarlis <nickgarlis@gmail.com>
2018-11-20 20:40:09 +0200

    Initial

---
+++ scripts/org-status.py
@@ -0,0 +1,33 @@
+import json
+import urllib2
+
+try:
+    res = urllib2.urlopen("https://api.github.com/orgs/ionian-uni-ieee/repos")
+except Exception as e:
+    print(e)
+    raise SystemExit
+
+try:
+    repos = json.load(res)
+except Exception as e:
+    print(e)
+    raise SystemExit
+
+try:
+    res.close()
+except Exception as e:
+    print(e)
+    raise SystemExit
+
+
+for repo in repos:
+    name = repo['name']
+    stars = repo['stargazers_count']
+    forks = repo['forks_count']
+
+    print('------------------------')
+    print('Name: ' + name)
+    print('Forks: ' + str(forks))
+    # add print statement here
+    print('------------------------')
+    print('')
